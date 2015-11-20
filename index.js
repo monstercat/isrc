026d17c82893d66075715f81419f9791e380574b
William Casarin <bill@casarin.me>
2015-11-19 16:35:36 -0800

    nodeify, remove component deps

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
 
-var DateTime = require("date-time");
-var Point = require("point");
-
 function Isrc ( opts ) {
+  if (!(this instanceof Isrc)) return new Isrc(opts)
   this.country = opts.country || "CC";
   this.registrant = opts.registrant || "XXX";
-  this.year = Point.clamp((parseInt(opts.year) || (new Date()).getFullYear()), 0, 9999);
-  this.id = Point.clamp((parseInt(opts.id) || 0), 0, 99999);
+  this.year = +opts.year || (new Date()).getFullYear();
+  this.id = +opts.id || 0;
 }
 
 Isrc.separator = "-";
-
-Isrc.prettify = function ( value ) {
-  var isrc = Isrc.parse(value);
-  return isrc.pretty();
-};
 
 Isrc.parse = function ( value ) {
   if ( typeof value == "string" ) {
     str = value.replace(Isrc.separator, "");
     return new Isrc({
-      country: str.substr(0, 2), 
-      registrant: str.substr(3, 3), 
+      country: str.substr(0, 2),
+      registrant: str.substr(3, 3),
       year: str.substr(5, 2),
       id: str.substr(7, 5)
     });
@@ -31,7 +24,7 @@ Isrc.parse = function ( value ) {
 };
 
 Isrc.parseYear = function ( year ) {
-  return DateTime.parseShortYear(year);
+  return parseInt(year.toString().match(/\d{2}$/)[0]);
 };
 
 Isrc.prototype.toString = function () {
@@ -62,19 +55,15 @@ Isrc.prototype.previous = function () {
 };
 
 Isrc.prototype.stringify = function ( id, separator ) {
-  id = id.toString();
+  id = id != null? id.toString() : this.id.toString();
   separator = separator || "";
   while ( id.length < 5 ) {
     id = "0" + id;
   }
   return (this.country + separator
     + this.registrant + separator
-    + Isrc.parseYear(this.year) + separator 
+    + Isrc.parseYear(this.year) + separator
     + id).toUpperCase();
-};
-
-Isrc.prototype.pretty = function () {
-  return this.stringify(this.id, Isrc.separator);
 };
 
 module.exports = Isrc;
